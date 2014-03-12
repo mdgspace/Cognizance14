dd60908a64e64b2fc3d3f6e00569067ffb5b9959
ankurgarg101 <ankurgarg101@gmail.com>
2014-03-13 00:48:34 +0530

    OnBackPressed Changes

---
+++ Cognizance/src/in/co/sdslabs/cognizance/HomeFragment.java
@@ -0,0 +1,18 @@
+package in.co.sdslabs.cognizance;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.home, 	container, false);
+	}
+
+}
