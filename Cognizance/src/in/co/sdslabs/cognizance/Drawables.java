3ec72505151df514ecc31e846b6bc685dbd276d2
Akshay Deshraj <akshay30nov@gmail.com>
2014-03-13 15:45:23 +0530

    Add drawables for a few categories

--- Cognizance/src/in/co/sdslabs/cognizance/Drawables.java
+++ Cognizance/src/in/co/sdslabs/cognizance/Drawables.java
@@ -3,12 +3,47 @@ package in.co.sdslabs.cognizance;
 public class Drawables {
 
 	// Array of integers points to images stored in /res/drawable-ldpi/
-	public static int[] navDrawerImages = new int[] { R.drawable.eventcat_theme,
-			R.drawable.eventcat_robotics, R.drawable.eventcat_competetions,
-			R.drawable.eventcat_literario, R.drawable.ic_launcher,
+	public static int[] navDrawerImages = new int[] {
+			R.drawable.eventcat_theme, R.drawable.eventcat_robotics,
+			R.drawable.eventcat_competetions, R.drawable.eventcat_literario,
 			R.drawable.ic_launcher, R.drawable.ic_launcher,
-			R.drawable.eventcat_csr, R.drawable.eventcat_efiesta,
-			R.drawable.eventcat_braintainment, R.drawable.eventcat_department,
-			R.drawable.ic_launcher };
+			R.drawable.ic_launcher, R.drawable.eventcat_csr,
+			R.drawable.eventcat_efiesta, R.drawable.eventcat_braintainment,
+			R.drawable.eventcat_department, R.drawable.ic_launcher };
+
+	public static int[][] eventsImages = new int[][] {
+
+			// Theme events
+			{ R.drawable.theme_construct_o_polis, R.drawable.theme_aurora,
+					R.drawable.theme_the_green_walk,
+					R.drawable.theme_sciennovate, R.drawable.theme_finesse,
+					R.drawable.theme_the_bron_identita },
+			// Robotics 
+					{},
+			//Competitions
+					{},
+			//Literario
+					{},
+			//Guest Lectures
+					{},
+			//Exhibitions
+					{},
+			//Techtainment
+					{},							
+			// CSR events
+			{ R.drawable.csr_rural_congress, R.drawable.csr_silence_calling },
+			
+			// E-fiesta events
+			{ R.drawable.efiesta_ignite, R.drawable.efiesta_elevator_pitch,
+					R.drawable.efiesta_techno_startup_showcase},
+			// Braintainment events
+			{},
+			
+			//Departments
+			{},
+			
+			//Contacts
+			{}
+	};
 
 }
