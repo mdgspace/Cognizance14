68e3d69993b66d01bcec818083738d7942aee3a2
Akshay Deshraj <akshay30nov@gmail.com>
2014-03-13 21:36:01 +0530

    Add drawables of different events

--- Cognizance/src/in/co/sdslabs/cognizance/Drawables.java
+++ Cognizance/src/in/co/sdslabs/cognizance/Drawables.java
@@ -6,10 +6,11 @@ public class Drawables {
 	public static int[] navDrawerImages = new int[] {
 			R.drawable.eventcat_theme, R.drawable.eventcat_robotics,
 			R.drawable.eventcat_competetions, R.drawable.eventcat_literario,
-			R.drawable.eventcat_guest_lectures, R.drawable.eventcat_exhibitions,
-			R.drawable.eventcat_techtainment, R.drawable.eventcat_csr,
-			R.drawable.eventcat_efiesta, R.drawable.eventcat_braintainment,
-			R.drawable.eventcat_department, R.drawable.ic_launcher };
+			R.drawable.eventcat_guest_lectures,
+			R.drawable.eventcat_exhibitions, R.drawable.eventcat_techtainment,
+			R.drawable.eventcat_csr, R.drawable.eventcat_efiesta,
+			R.drawable.eventcat_braintainment, R.drawable.eventcat_department,
+			R.drawable.ic_launcher };
 
 	public static int[][] eventsImages = new int[][] {
 
@@ -19,17 +20,27 @@ public class Drawables {
 					R.drawable.theme_sciennovate, R.drawable.theme_finesse,
 					R.drawable.theme_the_bron_identita },
 			// Robotics
-			{},
+			{ R.drawable.robotics_armageddon, R.drawable.robotics_powerdrift,
+					R.drawable.robotics_robosapeins,
+					R.drawable.robotics_cyborg_breakin,
+					R.drawable.robotics_aeronave },
 			// Competitions
-			{},
+			{ R.drawable.comp_chemecar, R.drawable.comp_junkyard_wars,
+					R.drawable.comp_corpostrat, R.drawable.comp_rubik_cube,
+					R.drawable.comp_chain_reaction, R.drawable.comp_chaos },
 			// Literario
-			{},
+			{ R.drawable.lit_iitr_mun, R.drawable.lit_vox_populi,
+					R.drawable.litquizzotica },
 			// Guest Lectures
 			{},
 			// Exhibitions
-			{},
+			{ R.drawable.ex_hand_gesture_controlled_devices,
+					R.drawable.ex_omnipresent, R.drawable.ex_gridbots,
+					R.drawable.ex_3d_printing, R.drawable.ex_graffiti,
+					R.drawable.ex_3doodler },
 			// Techtainment
-			{},
+			{ R.drawable.tech_shadow_act, R.drawable.tech_speed_painter,
+					R.drawable.tech_sunburn },
 			// CSR events
 			{ R.drawable.csr_rural_congress, R.drawable.csr_silence_calling },
 
@@ -37,7 +48,13 @@ public class Drawables {
 			{ R.drawable.efiesta_ignite, R.drawable.efiesta_elevator_pitch,
 					R.drawable.efiesta_techno_startup_showcase },
 			// Braintainment events
-			{},
+			{ R.drawable.br_block_buster, R.drawable.br_business_baazigar,
+					R.drawable.br_matrix_match,
+					R.drawable.br_principia_wierdis, R.drawable.br_sudoku,
+					R.drawable.br_whats_the_good_word, R.drawable.br_e_kanture,
+					R.drawable.br_paisa_parliament_policy
+
+			},
 
 			// Departments
 			{},
